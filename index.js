6baa5f35d448121ef5805cb2909d093b706ad1df
Leonard Kinday <leonard@kinday.ru>
2015-08-26 16:21:30 +0300

    feat(postcss-axis): add margin and padding shorthands

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 var postcss = require('postcss');
 
+function getAttrs(name, axis) {
+    var attrs = {
+        x: ['left', 'right'],
+        y: ['top', 'bottom']
+    };
+
+    return attrs[axis].map(function (side) {
+        return [name, side].join('-');
+    });
+}
+
 module.exports = postcss.plugin('postcss-axis', function (opts) {
     opts = opts || {};
 
-    // Work with options here
+    return function (css) {
+        var filter = /^(margin|padding)-(x|y)$/;
 
-    return function (css, result) {
+        css.walkDecls(filter, function (decl) {
+            var match = decl.prop.match(filter);
+            var attrs = getAttrs(match[1], match[2]);
+            var values = postcss.list.space(decl.value);
 
-        // Transform CSS AST here
+            if ( values.length === 1 ) values[1] = values[0];
 
+            decl.cloneBefore({ prop: attrs[0], value: values[0] });
+            decl.cloneBefore({ prop: attrs[1], value: values[1] });
+
+            decl.remove();
+        });
     };
 });
--- test/test.js
+++ test/test.js
@@ -15,10 +15,36 @@ var test = function (input, output, opts, done) {
 
 describe('postcss-axis', function () {
 
-    /* Write tests here
+    describe('margin', function () {
 
-    it('does something', function (done) {
-        test('a{ }', 'a{ }', { }, done);
-    });*/
+        it('sets margins', function (done) {
+            var input = 'a{ margin-x: 10px 20px; }';
+            var output = 'a{ margin-left: 10px; margin-right: 20px; }';
+            test(input, output, {}, done);
+        });
+
+        it('sets margins by one value', function (done) {
+            var input = 'a{ margin-x: 10px; }';
+            var output = 'a{ margin-left: 10px; margin-right: 10px; }';
+            test(input, output, {}, done);
+        });
+
+    });
+
+    describe('padding', function () {
+
+        it('sets paddings', function (done) {
+            var input = 'a{ padding-x: 10px 20px; }';
+            var output = 'a{ padding-left: 10px; padding-right: 20px; }';
+            test(input, output, {}, done);
+        });
+
+        it('sets paddings by one value', function (done) {
+            var input = 'a{ padding-x: 10px; }';
+            var output = 'a{ padding-left: 10px; padding-right: 10px; }';
+            test(input, output, {}, done);
+        });
+
+    });
 
 });
